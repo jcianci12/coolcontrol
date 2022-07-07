09871a443abe298ed5a9d08e0ed4bae65fae39a1
jcianci12 <jcianci12@gmail.com>
2022-07-08 07:00:57 +1000

    update. untested.

--- src/endpoints.h
+++ src/endpoints.h
@@ -6,11 +6,17 @@
 
 const char *PARAM_INPUT_1 = "input1";
 
-const size_t CAPACITY = JSON_OBJECT_SIZE(5);
-StaticJsonDocument<CAPACITY> doc;
+// const size_t CAPACITY = JSON_OBJECT_SIZE(5);
+// StaticJsonDocument<CAPACITY> doc;
 
 AsyncWebServer server(80);
 
+void mainUpdateJson()
+{
+    StaticJsonDocument<200> doc; // document has to be bigger than the space required for the output json text.
+    val = doc[PARAM_INPUT_1];
+    Serial.println(val);
+}
 
 void notFound(AsyncWebServerRequest *request)
 {
@@ -24,17 +30,26 @@ void initEndpoints()
 
     // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
 
-   AsyncCallbackJsonWebHandler* handler = new AsyncCallbackJsonWebHandler("/update", [](AsyncWebServerRequest *request, JsonVariant &json) {
+    //    AsyncCallbackJsonWebHandler* handler = new AsyncCallbackJsonWebHandler("/update", [](AsyncWebServerRequest *request, JsonVariant &json) {
+
+    //   doc = json.as<JsonObject>();
+    //   String world = doc[PARAM_INPUT_1];
+    // val = world.toInt();
+    //   // ...
+    // });
+
+    server.on("/update", [](AsyncWebServerRequest *request)
+              {
+  mainUpdateJson();
+  //request->send_P(200, "text/plain", "{done:success}");
+  //doc = json.as<JsonObject>();
+  //String world = doc[PARAM_INPUT_1];
   
-  doc = json.as<JsonObject>();
-  String world = doc[PARAM_INPUT_1];
-val = world.toInt();
-  // ...
-});
- server.addHandler(handler);
+//val = json[PARAM_INPUT_1];
+  // ... });
 
-}
-
+    // server.addHandler(handler); })
+})
 int readInputVal()
 {
     Serial.println(val);
